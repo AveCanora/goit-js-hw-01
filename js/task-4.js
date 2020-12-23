c04fdd9e5ed9d23c3c3bc3d1aa4f2c3cf895ef88
Natalya Taranenko <go.avecanora@gmail.com>
2020-12-23 13:22:23 +0200

    task-4

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+console.log('************TASK-4************');
+const credits = 23580;
+const pricePerDroid = 3000;
+let message;
+
+
+let quantity = prompt('Введите, пожалуйста, количество дроидов, которые вы хотите купить');
+if (quantity == null) message = 'Отменено пользователем!';
+else {
+
+    if (isNaN(Number(quantity)) == false) {
+        let totalPrice = quantity * pricePerDroid;
+        if (totalPrice > credits) message = 'Недостаточно средств на счету!'
+        else message = `Вы купили, ${quantity}, дроидов, на счету осталось,  ${credits - totalPrice}, кредитов!`;
+    }
+    else message = 'Вы ввели данные некоректно!';
+}
+
+alert(message);
