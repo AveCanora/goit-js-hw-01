f21e70919704ae1caeefb4223dfe6c6039835078
Natalya Taranenko <go.avecanora@gmail.com>
2020-12-24 19:22:37 +0200

    update

--- js/task-6.js
+++ js/task-6.js
@@ -1,25 +1,22 @@
 console.log('************TASK-6************');
 
-let input;
+
 let total = 0;
 let inputCheck = 0; /* промежуточная переменная для проверки введенных данных */
 // пока не нажата кнопка Отмена
-while (inputCheck != null) {
-    input = prompt(`Введите, пожалуйста, числа. Числа суммируются, пока не нажата кнопка "Отмена". Текущее значение суммы: ${total}`);
+while (true) {
+    let input = prompt(`Введите, пожалуйста, числа. Числа суммируются, пока не нажата кнопка "Отмена". Текущее значение суммы: ${total}`);
     // если нажали кнопку Отмена
     if (input != null) {
         if (input.trim().length == 0) inputCheck = Number('a'); /* проверка на введение пустого значения или пробела */
         else inputCheck = Number(input);  /* если введенное значение не пустое */
     }
     else { /* иначе: выход из цикла*/
-        inputCheck = null;
-        console.log(inputCheck);
         break;
     }
     if (isNaN(inputCheck) === false) { /*если введенное значение является числом */
         total += Number(input);
         inputCheck = Number(input);
-
     }
     else { /*иначе вводим значения, пока не введем число или не нажмем Отмена */
         while (isNaN(inputCheck) === true) {
@@ -33,7 +30,6 @@ while (inputCheck != null) {
                     }
                 }
             } else {/* иначе: выход из цикла*/
-                inputCheck = null;
                 break;
             }
 
